12e5f294c5fd80dcbc60414145262389204f2daf
mohd-sahill <mohammedsahil692@gmail.com>
2025-05-15 17:32:34 +0530

    Added new Email to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -90,6 +90,10 @@ const Footer = () => {
                 <Mail className="h-5 w-5 text-ozen-400 flex-shrink-0" />
                 <a href="mailto:mail@ozenconsultancy.com" className="hover:text-ozen-600 transition-colors">mail@ozenconsultancy.com</a>
               </div>
+               <div className="flex items-center space-x-3">
+                <Mail className="h-5 w-5 text-ozen-400 flex-shrink-0" />
+                <a href="mailto:mail@ozenconsultancy.com" className="hover:text-ozen-600 transition-colors">support@ozenconsultancy.com</a>
+              </div>
               <div className="flex items-center space-x-3">
                 <Phone className="h-5 w-5 text-ozen-400 flex-shrink-0" />
                 <a href="tel:+919902392311" className="hover:text-ozen-600 transition-colors">+91 99023 99211</a>
